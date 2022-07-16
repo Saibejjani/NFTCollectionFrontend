6bf5b3406654be440c7093e72b269c6f35c91d50
Saibejjani <64737059+Saibejjani@users.noreply.github.com>
2022-07-16 22:57:34 +0530

    second commit for utility

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+import genAbi from "./NFTGenrator.json";
+import colAbi from "./NFTContract.json";
+
+export const contractAddress = "0x61c36a8d610163660E21a8b7359e1Cac0C9133e1";
+
+export const GenratorABI = genAbi.abi;
+export const CollectionABI = colAbi.abi;
