3cbad2642a35377eaf895cd4300c98be6ef74db7
Saibejjani <64737059+Saibejjani@users.noreply.github.com>
2022-07-18 23:39:01 +0530

    Update TransactionContext.jsx

--- src/context/TransactionContext.jsx
+++ src/context/TransactionContext.jsx
@@ -66,6 +66,133 @@ export const TransactionProvider = ({ children }) => {
     }
   };
 
+  const fetchColletions = async () => {
+    console.log(currentColletion);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          contractAddress,
+          NFTGen,
+          signer
+        );
+
+        const len = await connectedContract.collectionsLength();
+
+        for (let i = fetched; i < len.toNumber(); i++) {
+          const currentAddress = await connectedContract.collections(i);
+          setCurrentContractsAddress((current) => [...current, currentAddress]);
+          const currentContract = new ethers.Contract(
+            currentAddress,
+            NFTCol,
+            signer
+          );
+          let uri = await currentContract.tokenURI(1);
+          uri = `https://ipfs.io/ipfs/${uri.split("ipfs://")[1]}`;
+          const tokenMetadata = await fetch(uri).then((response) =>
+            response.json()
+          );
+          let { image } = tokenMetadata;
+
+          image = `https://ipfs.io/ipfs/${image.split("ipfs://")}`;
+          image = image.replace(",", "");
+          const name = await currentContract.name();
+          const symbol = await currentContract.symbol();
+
+          setTotalColletions((current) => [
+            ...current,
+            { image, name, symbol },
+          ]);
+        }
+
+        setFetched(len.toNumber());
+        console.log(currentContractAddress);
+        console.log(len.toNumber());
+      } else {
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchThisCollection = async (index) => {
+    try {
+      const { ethereum } = window;
+
+      if (prev !== index) {
+        setPrev(index);
+
+        setCurrentCollection([]);
+        setFetchLimit(0);
+      }
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const currentContract = new ethers.Contract(
+          currentContractAddress[index],
+          NFTCol,
+          signer
+        );
+        console.log(currentContractAddress[index]);
+        const totalSupply = await currentContract.totalSupply();
+
+        for (let i = fetchLimit + 1; i <= totalSupply.toNumber(); i++) {
+          let uri = await currentContract.tokenURI(i);
+          uri = `https://ipfs.io/ipfs/${uri.split("ipfs://")[1]}`;
+          const tokenMetadata = await fetch(uri).then((response) =>
+            response.json()
+          );
+          let { image, name, description } = tokenMetadata;
+
+          image = `https://ipfs.io/ipfs/${image.split("ipfs://")}`;
+          image = image.replace(",", "");
+          setCurrentCollection((current) => [
+            ...current,
+            { name, description, image },
+          ]);
+          console.log(image);
+        }
+        setFetchLimit(totalSupply.toNumber());
+      } else {
+        console.log("ethereum object not found");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const MintCollection = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          contractAddress,
+          NFTGen,
+          signer
+        );
+        const { Name, Symbol, URI, MintAmount } = formData;
+
+        const rec = await connectedContract.createNewCollection(
+          Name,
+          Symbol,
+          URI,
+          MintAmount
+        );
+        const trx = await rec.wait();
+        console.log(trx);
+        fetchColletions();
+      } else {
+        console.log("Ethereum object not found");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
